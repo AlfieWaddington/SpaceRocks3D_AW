0c117057742411f00996effa1ccd8adba18e7279
gls22019258 <gls22019258@C22000026>
2026-02-02 20:31:35 +0000

    Added basic ship movement

--- Source/SpaceRocks3D_AW/AAWSpaceShip.cpp
+++ Source/SpaceRocks3D_AW/AAWSpaceShip.cpp
@@ -5,6 +5,7 @@
 
 // Sets default values
 AAAWSpaceShip::AAAWSpaceShip()
+
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -20,7 +21,11 @@ AAAWSpaceShip::AAAWSpaceShip()
 	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("The Spring Arm"));
 	SpringArm->SetupAttachment(RootComponent);
 	SpringArm->TargetArmLength = 800;
-	SpringArm->SetRelativeLocation(FVector(-800, 0, 300));
+	SpringArm->SetRelativeLocation(FVector(-600, 0, 230));
+	SpringArm->AddRelativeRotation(FRotator(-7, 0, 0));
+	SpringArm->bEnableCameraLag = true;
+	SpringArm->bEnableCameraRotationLag = true;
+
 
 	// Create the UCameraComponent and attach it to the spring arm,
 	// this means that its transform is relative to the spring arm
@@ -32,7 +37,8 @@ AAAWSpaceShip::AAAWSpaceShip()
 	TheShip = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("The Mesh"));
 	TheShip->SetupAttachment(RootComponent);
 
-
+	//Automatically Possess the Player Controller
+	AutoPossessPlayer = EAutoReceiveInput::Player0; //We only have 1 player and it's Player0
 }
 
 // Called when the game starts or when spawned
@@ -47,12 +53,177 @@ void AAAWSpaceShip::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	//Set the accelration to zero at the start of each frame
+	Acceleration = FVector3d(0.0f, 0.0f, 0.0f);
+
+	// Turn (rotate around the Z axis - Yaw) the ship if A or D is held
+	if (TurnDirection != 0)
+	{
+		FRotator Rotation = GetActorRotation();
+		Rotation.Yaw = Rotation.Yaw + TurnDirection * RotationSpeed * DeltaTime;  // Yaw is rotation around the up/down axis this is the Z axis in Unreal
+		SetActorRotation(Rotation);
+	}
+
+	// Turn (rotate around the Y axis - Pitch) the ship if UpArrow or DownArrow is held
+	if (AltitudeDirection != 0)
+	{
+		FVector3d UpVector = GetActorUpVector(); //Get the upward direction of the ship
+		//Multiply the UpVector by the direction (based on the up/down keys) and by the Altitude Thrust strength (which is tunable in the editor)
+		Acceleration = Acceleration + UpVector * AltitudeDirection * AltitudeThrustStrength;
+	}
+
+	// Calculate and add forward acceleration if thrusting
+	if (bIsThrusting)
+	{
+		//Modify the acceleration for this frame
+		FVector3d Forward = GetActorForwardVector();  //Get the direction the ship is pointing in
+		Acceleration = Acceleration + Forward * ThrustStrength;  //Set the acceleration based on the direction of the ship
+	}
+
+	
+
+	// Update velocity using acceleration
+	Velocity = Velocity + Acceleration * DeltaTime;
+
+	//Add to friction if breaking if not use normal friction
+	float friction = bIsBraking ? BrakingFriction : Friction;
+
+	// Apply friction (drag)
+	Velocity = Velocity - (Velocity * friction);
+
+	// Cap the speed to MaxSpeed
+	if (Velocity.Size() > MaxSpeed)
+	{
+		Velocity = Velocity.GetClampedToMaxSize(MaxSpeed);
+	}
+
+	printSpeed();
+
+	// Move the ship using the current velocity
+	AddActorWorldOffset(Velocity * DeltaTime, true);
+
 }
 
 // Called to bind functionality to input
 void AAAWSpaceShip::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	APawn::SetupPlayerInputComponent(PlayerInputComponent);
+
+	//When you override a method from the Parent class always call the parents implementation to get the default behaviour
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Bind W key for thrust
+	PlayerInputComponent->BindKey(EKeys::W, IE_Pressed, this, &AAAWSpaceShip::OnThrustPressed);
+	PlayerInputComponent->BindKey(EKeys::W, IE_Released, this, &AAAWSpaceShip::OnThrustReleased);
+
+	// Bind A and D keys for turning
+	PlayerInputComponent->BindKey(EKeys::A, IE_Pressed, this, &AAAWSpaceShip::OnTurnLeftPressed);
+	PlayerInputComponent->BindKey(EKeys::D, IE_Pressed, this, &AAAWSpaceShip::OnTurnRightPressed);
+
+	// Stop turning when A or D is released
+	PlayerInputComponent->BindKey(EKeys::A, IE_Released, this, &AAAWSpaceShip::OnTurnReleased);
+	PlayerInputComponent->BindKey(EKeys::D, IE_Released, this, &AAAWSpaceShip::OnTurnReleased);
+
+	// Bind S key for break
+	PlayerInputComponent->BindKey(EKeys::S, IE_Released, this, &AAAWSpaceShip::OnBrakePressed);
+	PlayerInputComponent->BindKey(EKeys::S, IE_Released, this, &AAAWSpaceShip::OnBrakeReleased);
+
+	// Bind UpArrow and DownArrow keys for turning
+	PlayerInputComponent->BindKey(EKeys::Up, IE_Pressed, this, &AAAWSpaceShip::OnAscendPressed);
+	PlayerInputComponent->BindKey(EKeys::Down, IE_Pressed, this, &AAAWSpaceShip::OnDescendPressed);
+
+	// Stop turning when UpArrow or DownArrow is released
+	PlayerInputComponent->BindKey(EKeys::Up, IE_Released, this, &AAAWSpaceShip::OnAltitudeReleased);
+	PlayerInputComponent->BindKey(EKeys::Down, IE_Released, this, &AAAWSpaceShip::OnAltitudeReleased);
 
 }
 
+void AAAWSpaceShip::SetInputEnabled(bool bIsEnabled)
+{
+	bCanReceiveInput = bIsEnabled;
+}
+
+void AAAWSpaceShip::printSpeed()
+{
+	FString speedOut = FString(TEXT("Speed: "));
+	FString vel = FString::FromInt((int)Velocity.Size());
+	FString out = speedOut + vel;
+	GEngine->AddOnScreenDebugMessage(0, 1.0f, FColor(1, 1, 1, 255), out);
+}
+
+void AAAWSpaceShip::OnThrustPressed()
+{
+	//If input isn't enabled return early
+	if (!bCanReceiveInput)
+	{
+		return;
+	}
+	bIsThrusting = true;
+
+}
+
+void AAAWSpaceShip::OnThrustReleased()
+{
+	bIsThrusting = false;
+}
+
+void AAAWSpaceShip::OnTurnLeftPressed()
+{
+	//If input isn't enabled return early
+	if (!bCanReceiveInput)
+	{
+		return;
+	}
+	TurnDirection = -1;
+}
+
+void AAAWSpaceShip::OnTurnRightPressed()
+{
+	//If input isn't enabled return early
+	if (!bCanReceiveInput)
+	{
+		return;
+	}
+	TurnDirection = 1;
+}
+
+void AAAWSpaceShip::OnTurnReleased()
+{
+	TurnDirection = 0;
+
+}
+
+void AAAWSpaceShip::OnBrakePressed()
+{
+	bIsBraking = true;
+}
+
+void AAAWSpaceShip::OnBrakeReleased()
+{
+	bIsBraking = false;
+}
+
+void AAAWSpaceShip::OnAscendPressed()
+{
+	//If input isn't enabled return early
+	if (!bCanReceiveInput)
+	{
+		return;
+	}
+	AltitudeDirection = 1;
+}
+
+void AAAWSpaceShip::OnDescendPressed()
+{
+	//If input isn't enabled return early
+	if (!bCanReceiveInput)
+	{
+		return;
+	}
+	AltitudeDirection = -1;
+}
+
+void AAAWSpaceShip::OnAltitudeReleased()
+{
+	AltitudeDirection = 0;
+
+}
--- Source/SpaceRocks3D_AW/AAWSpaceShip.h
+++ Source/SpaceRocks3D_AW/AAWSpaceShip.h
@@ -21,6 +21,30 @@ public:
 	// Sets default values for this pawn's properties
 	AAAWSpaceShip();
 
+private:
+	//These five member variables don't need to be exposed to the Unreal Editor
+	FVector Velocity = FVector(0.0f, 0.0f, 0.0f);
+	FVector Acceleration = FVector(0.0f, 0.0f, 0.0f);
+
+	// Input state
+	bool bCanReceiveInput = true;  //This will allow us to disable and enable the controlls, it will be useful later
+	bool bIsThrusting = false;
+	bool bIsBraking = false;
+	int AltitudeDirection = 0;	// -1 = down, 1 = up, 0 = none
+	int  TurnDirection = 0;      // -1 = left, 1 = right, 0 = none
+
+	// Input handler method declarations
+	void OnThrustPressed();
+	void OnThrustReleased();
+	void OnTurnLeftPressed();
+	void OnTurnRightPressed();
+	void OnTurnReleased();
+	void OnBrakePressed();
+	void OnBrakeReleased();
+	void OnAscendPressed();
+	void OnDescendPressed();
+	void OnAltitudeReleased();
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -41,11 +65,54 @@ protected:
 	UPROPERTY(VisibleAnywhere)
 	UStaticMeshComponent* TheShip;
 
+	//The maximum speed: 
+	//We've made this editable from within the Unreal Editor
+	//In the Editor you will find these in the details panel under a "Movement" category
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float MaxSpeed = 3500.0f;
+
+	//The ThrustStrength will detemine the acceleration: 
+	//We've made this editable from within the Unreal Editor
+	//In the Editor you will find these in the details panel under a "Movement" category
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float ThrustStrength = 800.0f;
+
+	//The AltitudeThrustStrength will detemine the vertical acceleration: 
+	//We've made this editable from within the Unreal Editor
+	//In the Editor you will find these in the details panel under a "Movement" category
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float AltitudeThrustStrength = 600.0f;
+
+	//The Friction: 
+	//We've made this editable from within the Unreal Editor
+	//In the Editor you will find these in the details panel under a "Movement" category
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float Friction = 0.001f;
+
+	//The Breaking Friction (higher = stronger brakes): 
+	//We've made this editable from within the Unreal Editor
+	//In the Editor you will find these in the details panel under a "Movement" category
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float BrakingFriction = 0.003f;
+
+	//The Rotation speed: 
+	//We've made this editable from within the Unreal Editor
+	//In the Editor you will find these in the details panel under a "Movement" category
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float RotationSpeed = 30.0f;
+
+
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	//Setter for enabling/disabling the input controls
+	void SetInputEnabled(bool bIsEnabled);
+
+	void printSpeed();
 
 };
